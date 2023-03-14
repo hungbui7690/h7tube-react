7db7215523ec848c399ab9a790d45529e0fcf959
Hung Bui <hungbui7690@gmail.com>
2023-03-14 17:42:23 -0600

    React: Structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,37 @@
-import './App.css'
+/*
+  - npm install react-router-dom styled-components
+
+  - create Container
+    - create Menu (left side of Youtube, when in maximized mode)
+    - create Main (right side)
+      - create Wrapper (this one is the wrapper for all video cards)
+
+
+*/
+
+import styled from 'styled-components'
+import Menu from './components/Menu'
+import Navbar from './components/Navbar'
+
+// (1)
+const Container = styled.div``
+
+// (3)
+const Main = styled.div``
+
+// (5)
+const Wrapper = styled.div``
 
 function App() {
-  return <div>App</div>
+  return (
+    <Container>
+      <Menu />
+      <Main>
+        <Navbar />
+        <Wrapper>Video Cards</Wrapper>
+      </Main>
+    </Container>
+  )
 }
 
 export default App
